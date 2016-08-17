e28b94fe3aee6c74fe7ec830ef0ec2e01559fd11
Cornelius Buschka <cbuschka@users.noreply.github.com>
2016-08-17 20:30:22 +0200

    Added more tests

--- src/test/python/versioned/version_tests.py
+++ src/test/python/versioned/version_tests.py
@@ -17,6 +17,19 @@ class VersionTestCase(unittest.TestCase):
 
   def testIsVersionWithFullVersionInfixAndSnapshot(self):
     self.assertTrue(self.versionBuilder.build('1.2.3-RC-4-SNAPSHOT').isVersion())
+  
+  def testNextReleaseOfSnapshotRemovesSnapshot(self):
+    self.assertEquals('1.2.3', self.versionBuilder.build('1.2.3-SNAPSHOT').nextRelease().str())
+  
+  def testNextReleaseOfReleaseIncreasesPatch(self):
+    self.assertEquals('1.2.4', self.versionBuilder.build('1.2.3').nextRelease().str())
+  
+  def testNextSnapshotOfReleaseIncreasesPatchAndAddsSnapshot(self):
+    self.assertEquals('1.2.4-SNAPSHOT', self.versionBuilder.build('1.2.3').nextSnapshot().str())
+  
+  def testNextSnapshotOfSnapshotIncreasesPatchAndKeepsSnapshot(self):
+    self.assertEquals('1.2.4-SNAPSHOT', self.versionBuilder.build('1.2.3-SNAPSHOT').nextSnapshot().str())
+
 
 if __name__ == '__main__':
     unittest.main()
