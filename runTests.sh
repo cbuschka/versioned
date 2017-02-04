d2d4c0862091bd115185095bc8afc73250566d9e
conni <c.buschka@arcusx.com>
2017-02-04 07:52:56 +0100

    Added support for test coverage.

--- runTests.sh
+++ runTests.sh
@@ -4,4 +4,4 @@ BASE_DIR=$(dirname $0)
 
 export PYTHONPATH=$BASE_DIR/src/main/python:$BASE_DIR/src/test/python:$PYTHONPATH
 
-python $BASE_DIR/src/test/python/versioned/version_tests.py -v
+coverage run -m unittest discover -s $BASE_DIR/src/test/python/versioned -v -p '*_test?.py'
